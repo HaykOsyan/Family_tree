dfe4a0bbae12fc11ae22688bbcb426f105e7276c
«HaykOsyan <ohayk@mail.ru>
2024-10-30 11:39:49 +0400

    V3.03 Main java merged packages

--- Main.java
+++ Main.java
@@ -5,8 +5,25 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         FamilyTree familyTree = new FamilyTree();
-        
 
+        familyTree.addPerson("John", "male");
+        familyTree.addPerson("Mary", "female");
+        familyTree.addChild("John", "Anna", "female");
+
+        List<Person> sortedPeople = Operations.sortByName(familyTree);
+        System.out.println("Sorted people: " + sortedPeople);
+
+        // Save to file
+        Operations.saveToFile(familyTree, "familyTree.dat");
+
+        // Load from file
+        FamilyTree loadedFamilyTree = Operations.loadFromFile("familyTree.dat");
+        if (loadedFamilyTree != null) {
+            System.out.println("Loaded family tree: " + loadedFamilyTree.getAllPeople());
+        } else {
+            System.out.println("Failed to load family tree.");
+        }
     }
 }
+
 
